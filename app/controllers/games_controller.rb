e9c83a99032f323298e575b943a5509252b0e39e
pierreuntas <pierre.untas@gmail.com>
2022-11-22 17:29:35 +0100

    games controller / games views new / css games new

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,20 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.all
   end
 
   def show
+    @game = Game.find(params[:id])
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.new(game_params)
+    @game.save
+    redirect_to game_path(game)
   end
 
   def edit
@@ -19,4 +25,11 @@ class GamesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :description, :type, :level, :player, :age, :price)
+  end
+
 end
