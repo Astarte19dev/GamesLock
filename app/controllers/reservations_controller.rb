a35b014ce94b9e002e1394c88eff2d5b11c2b085
aude cateland <aude.cateland@gmail.com>
2022-11-22 12:48:12 +0100

    start reservation new

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -6,9 +6,13 @@ class ReservationsController < ApplicationController
   end
 
   def new
+    @game = Game.find(params[:game_id])
+    @reservation = Reservation.new
   end
 
   def create
+    @game = Game.find(params[:game_id])
+    @reservation = Reservation.new(reservation_param)
   end
 
   def destroy
