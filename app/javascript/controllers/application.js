fe5f3f1bd24107f429bab41ffa9d4a9ac30a5703
mathieu chirac <astarte19dev@gmail.com>
2022-11-23 16:47:12 +0100

    fix

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,17 +7,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-
-<div data-controller="refresh-list">
-  <%= form_with url: movies_path, method: :get, html: { class: 'mb-5', data: { refresh_list_target: 'form' } } do |f| %>
-    <%= f.text_field :query,
-                     class: "form-control",
-                     placeholder: 'Type a movie title',
-                     value: params[:query],
-                     data: {
-                      refresh_list_target: 'searchInput',
-                    } %>
-  <% end %>
-  <div data-refresh-list-target="list">
-  </div>
-</div>
