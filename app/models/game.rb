2ca5c51dc8c0f98c725e1d2d3fc25449a59dcee4
aude cateland <aude.cateland@gmail.com>
2022-11-24 15:32:44 +0100

    version diff with last master

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,4 +5,11 @@ class Game < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
   has_many :reservations, dependent: :destroy
+  validates :name, presence: true
+  validates :description, presence: true
+  # validates :game_type, presence: true
+  validates :level, presence: true
+  validates :player, presence: true
+  validates :age, presence: true
+  validates :price, presence: true
 end
