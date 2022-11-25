6e5621249e1809ec47651a2a787b9d0134ab8bf0
mathieu chirac <astarte19dev@gmail.com>
2022-11-25 11:59:52 +0100

    debug delete edit form

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_111803) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "controllers_to_apps", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "games", force: :cascade do |t|
     t.string "name"
     t.string "description"
